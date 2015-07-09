5f93394a0955b47dfc488402bed032eb4d0347a4
藤井 裕子 <y_fujii@y-fujii.local>
2015-07-09 20:49:39 +0900

    初期

---
+++ ciSample/ciSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ciSample
+//
+//  Created by 藤井 裕子 on 2015/07/09.
+//  Copyright (c) 2015年 Hakuhodo i-studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
